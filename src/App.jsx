b430994aeb6c3d62bd763f4d87e4fbeb6d658737
Estefania <148402713+estefaniagonz@users.noreply.github.com>
2024-10-11 00:36:20 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from './components/Header/Header'; 
-import TaskInput from "./components/Header/TaskInput/TaskInput";
+import TaskInput from "./components/TaskInput/TaskInput";
 import ('./App.css')
 
 const App = () => {
