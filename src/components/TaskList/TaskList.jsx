043f9956e196f1337e363995d3bfed3c9a27ed35
Estefania <148402713+estefaniagonz@users.noreply.github.com>
2024-10-14 20:08:47 -0500

    actualización de errores en tasklist

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -21,26 +21,3 @@ const TaskList = ({ tasks, toggleTask, editTask, deleteTask }) => {
 };
 
 export default TaskList;
-import React from 'react';
-import TaskItem from '../TaskItem/TaskItem';
-import './TaskList.css';
-
-const TaskList = ({ tasks, toggleTask, editTask, deleteTask }) => {
-  return (
-    <div className="task-list-container">
-      <ul className="task-list">
-        {tasks.map(task => (
-          <TaskItem
-            key={task.id}
-            task={task}
-            toggleTask={toggleTask}
-            editTask={editTask}
-            deleteTask={deleteTask}
-          />
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-export default TaskList;
