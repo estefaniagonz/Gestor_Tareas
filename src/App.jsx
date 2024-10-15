51f806ab16e8557571177e094d35ec63ef98cccf
Estefania <148402713+estefaniagonz@users.noreply.github.com>
2024-10-14 19:22:30 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-<import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TaskList from './components/TaskList/TaskList.jsx';
 import Header from './components/Header/Header';
